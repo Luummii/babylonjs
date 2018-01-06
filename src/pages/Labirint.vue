31334dd7fced31888bff8d810e8033da123c4aa6
Andrey Zimin (luummii) <luummii@vuejs.fun>
2018-01-06 19:57:08 +0300

    ++

--- src/pages/Labirint.vue
+++ src/pages/Labirint.vue
@@ -21,7 +21,10 @@ export default {
     const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, -8, -20), scene)   
     camera.attachControl(canvas, true)
 
-    const ground = BABYLON.Mesh.CreatePlane('ground', 20.0, scene);
+    const light1 = new BABYLON.DirectionalLight('Omni', new BABYLON.Vector3(-2, -5, 2), scene)
+    const light2 = new BABYLON.PointLight('Omni', new BABYLON.Vector3(2, -5, -2), scene)
+
+    const ground = BABYLON.Mesh.CreatePlane('ground', 1000.0, scene);
     ground.material = new BABYLON.StandardMaterial('groundMat', scene);
     ground.material.diffuseColor = new BABYLON.Color3(1, 1, 1);
     ground.material.backFaceCulling = false;
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ import BallAnimation from '../pages/BallAnimation.vue'
 import MoveAnimation from '../pages/MoveAnimation.vue'
 import ClashOfBalls from '../pages/ClashOfBalls.vue'
 import Gravity from '../pages/Gravity.vue'
-import Colors from '../pages/Colors.vue'
+import Labirint from '../pages/Labirint.vue'
 
 export default [
   { path: '/Ball', name: 'Ball', component: Ball },
@@ -19,6 +19,6 @@ export default [
   { path: '/MoveAnimation', name: 'Move Animation', component: MoveAnimation },
   { path: '/ClashOfBalls', name: 'Clash Of Balls', component: ClashOfBalls },
   { path: '/Gravity', name: 'Gravity', component: Gravity },
-  { path: '/Colors', name: 'Colors', component: Colors },
+  { path: '/Labirint', name: 'Labirint', component: Labirint },
   { path: '*', redirect: 'Ball' }
 ]
