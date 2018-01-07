81eddff7bcaff0319bcd51320d1ad722c4eecfa2
Andrey Zimin (luummii) <luummii@vuejs.fun>
2018-01-07 19:39:34 +0300

    ++

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ import BallAnimation from '../pages/BallAnimation.vue'
 import MoveAnimation from '../pages/MoveAnimation.vue'
 import ClashOfBalls from '../pages/ClashOfBalls.vue'
 import Gravity from '../pages/Gravity.vue'
-import FootballLight from '../pages/FootballLight.vue'
+import FootballAnimation from '../pages/FootballAnimation.vue'
 
 export default [
   { path: '/Ball', name: 'Ball', component: Ball },
@@ -19,6 +19,6 @@ export default [
   { path: '/MoveAnimation', name: 'Move Animation', component: MoveAnimation },
   { path: '/ClashOfBalls', name: 'Clash Of Balls', component: ClashOfBalls },
   { path: '/Gravity', name: 'Gravity', component: Gravity },
-  { path: '/FootballLight', name: 'Football Light', component: FootballLight },
+  { path: '/FootballAnimation', name: 'Football Animation', component: FootballAnimation },
   { path: '*', redirect: 'Ball' }
 ]
